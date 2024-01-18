92725d23b423ec8fa618f6aba60a116dad8fccb3
Jayt4c <igejuwon53@gmail.com>
2024-01-18 12:48:37 -0800

    sub function updated

--- monty.h
+++ monty.h
@@ -49,6 +49,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_n);
 void add(stack_t **top_ptr, unsigned int line_n);
 void nop(stack_t **stack, unsigned int line_n);
+void sub(stack_t **stack, unsigned int line_n);
 void exec_op(char *tokens[], stack_t **stack, unsigned int line_number);
 void freestack(stack_t *head);
 
--- ops.c
+++ ops.c
@@ -130,11 +130,25 @@ void nop(stack_t **stack, unsigned int line_n)
 	line_n = line_n;
 }
 
+void sub(stack_t **stack, unsigned int line_n)
+{
+	stack_t *top = *stack;
+
+	if (!top || !top->next)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_n);
+		exit(EXIT_FAILURE);
+	}
+
+	top->next->n -= top->n;
+	pop(stack, line_n);
+}
+
 
 void exec_op(char *tokens[], stack_t **stack, unsigned int line_number)
 {
 	int i, length;
-	instruction_t op_ar[] = {{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}};
+	instruction_t op_ar[] = {{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub}};
 
 	length = sizeof(op_ar) / sizeof(op_ar[0]);
 
